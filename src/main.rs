2f5f7b8f0954a6f75f6f8900336aad894cfba300
User Name <3958499+dariuszk@users.noreply.github.com>
2022-11-03 19:34:17 +0100

    added tower file, fix camera vector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
 
+mod tower;
+
 pub const HEIGHT: f32 = 800.;
 pub const WIDTH: f32 = 1920.;
-pub const GAME_NAME: &str =  "Rusty Defender" ;
+pub const GAME_NAME: &str = "Rusty Defender";
 
 fn main() {
     App::new()
@@ -24,39 +26,34 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-)
-{
-    const PLANE_COLOR : Color = Color::rgb(0.6, 0.0, 0.75);
-    const PLANE_SIZE :  f32 = 10.;
+) {
+    const PLANE_COLOR: Color = Color::rgb(0.6, 0.0, 0.75);
+    const PLANE_SIZE: f32 = 10.;
 
     // Setup plane
-        commands
-            .spawn_bundle(
-                PbrBundle {
-                                mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE_SIZE })),
-                                material: materials.add(PLANE_COLOR.into()),
-                                ..default()
-                                }
-            );
-        // Light
-        commands
-            .spawn_bundle(
-                PointLightBundle {
-                    point_light: PointLight {
-                        intensity: 1500.0,
-                        shadows_enabled: true,
-                        ..default()
-                    },
-                transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-                ..Default::default()
-            });
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE_SIZE })),
+        material: materials.add(PLANE_COLOR.into()),
+        ..default()
+    });
+    // Light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn_bundle(
-                     Camera3dBundle {
-                                transform: Transform::from_xyz(8.5, 4., 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-                                ..default()
-                            }
-    );
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-6.0, 10.0, 0.0),
+        )),
+        ..default()
+    });
 }
